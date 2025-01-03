3cc8a630dee03230554c5dd2d8ee0e976d922259
Namo2202 <nafise.mougamadousalime@gmail.com>
2025-01-03 17:55:14 +0100

    Conditional Tests - @EnabledOnJre and @EnabledForJreRange

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -2,8 +2,7 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
 
 public class ConditionalTest {
 
@@ -22,6 +21,42 @@ public class ConditionalTest {
     @Test
     @EnabledOnOs({OS.LINUX, OS.WINDOWS})
     void testForLinuxAndWindowsOnly() {
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_17)
+    void testForOnlyJava17() {
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_21)
+    void testForOnlyJava21() {
+
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_13, max=JRE.JAVA_17)
+    void testOnlyForJavaRange() {
+
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_21, max=JRE.JAVA_23)
+    void testOnlyForJavaRange2() {
+
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_11)
+    void testOnlyForJavaRangeMin() {
+
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_23)
+    void testOnlyForJavaRangeMax() {
 
     }
 }
