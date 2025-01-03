978c3ad8278500003d012e16de9ca6c00b4401d4
Namo2202 <nafise.mougamadousalime@gmail.com>
2025-01-03 10:37:25 +0100

    Test Case Defining Custom Display Names

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class DemoUtilsTest {
 
     DemoUtils demoUtils;
@@ -14,38 +15,37 @@ public class DemoUtilsTest {
         System.out.println("@BeforeEach executes before the execution of each test method");
     }
 
-    @AfterEach
-    void tearDownAfterEach() {
-        System.out.println("Running @AfterEach");
-        System.out.println();
-    }
-
-    @BeforeAll
-    static void setBeforeEachClass() {
-        System.out.println("@BeforeAll executes only once before all test methods execution in the class");
-    }
-
-    @AfterAll
-    static void tearDownAfterAll() {
-        System.out.println("@AfterAll executes only once after all test methods execution in the class");
-    }
-
     @Test
-    void testEqualsAndNotEquals() {
-        System.out.println("Running test: testEqualsAndNotEquals");
-
+//    @DisplayName("Equals and Not Equals")
+    void test_Equals_And_Not_Equals() {
         assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1,9), "2+4 must not be 6");
     }
 
     @Test
-    void testNullAndNotNull() {
-        System.out.println("Running test: testNullAndNotNull");
-
+//    @DisplayName("Null and Not Null")
+    void test_Null_And_Not_Null() {
         String str1 = null;
         String str2 = "luv2code";
 
         assertNull(demoUtils.checkNull(str1), "object should be null");
         assertNotNull(demoUtils.checkNull(str2), "object should not be null");
     }
+
+//
+//    @AfterEach
+//    void tearDownAfterEach() {
+//        System.out.println("Running @AfterEach");
+//        System.out.println();
+//    }
+//
+//    @BeforeAll
+//    static void setBeforeEachClass() {
+//        System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+//    }
+//
+//    @AfterAll
+//    static void tearDownAfterAll() {
+//        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+//    }
 }
