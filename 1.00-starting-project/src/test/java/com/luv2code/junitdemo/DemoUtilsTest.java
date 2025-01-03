7c73d09ee229812d30573e399420f737b08bfea0
Namo2202 <nafise.mougamadousalime@gmail.com>
2025-01-03 11:35:27 +0100

    JUnit Assertions - Arays, Iterable, Lines

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,8 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
@@ -46,6 +48,27 @@ public class DemoUtilsTest {
         int grade2 = 5;
         assertTrue(demoUtils.isGreater(grade1, grade2), "This should return true");
         assertFalse(demoUtils.isGreater(grade2, grade1), "This should return false");
+    }
+
+    @Test
+    @DisplayName("Array Equals")
+    void arrayEquals() {
+        String[] stringArray = {"A","B","C"};
+        assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be the same");
+    }
+
+    @Test
+    @DisplayName("Iterable equals")
+    void testIterableEquals() {
+        List<String> theList = List.of("luv", "2", "code");
+        assertIterableEquals(theList, demoUtils.getAcademyInList(), "Expected list should be same as actual list");
+    }
+
+    @Test
+    @DisplayName("Lines match")
+    void testLinesMatch() {
+        List<String> theList = List.of("luv", "2", "code");
+        assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
     }
 
 /*    @AfterEach
