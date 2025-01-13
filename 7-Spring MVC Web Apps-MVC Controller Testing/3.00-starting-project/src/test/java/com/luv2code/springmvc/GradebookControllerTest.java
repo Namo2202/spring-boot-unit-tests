bccd272c17a7d369e9cdc380b2fadc551a47d636
Namo2202 <nafise.mougamadousalime@gmail.com>
2025-01-13 16:38:51 +0100

    set up SQL Scripts in properties file

--- 7-Spring MVC Web Apps-MVC Controller Testing/3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 7-Spring MVC Web Apps-MVC Controller Testing/3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -49,6 +50,30 @@ public class GradebookControllerTest {
     @Autowired
     private StudentDao studentDao;
 
+    @Value("${sql.script.create.student}")
+    private String sqlAddStudent;
+
+    @Value("${sql.script.create.math.grade}")
+    private String sqlAddMathGrade;
+
+    @Value("${sql.script.create.science.grade}")
+    private String sqlAddScienceGrade;
+
+    @Value("${sql.script.create.history.grade}")
+    private String sqlAddHistoryGrade;
+
+    @Value("${sql.script.delete.student}")
+    private String sqlDeleteStudent;
+
+    @Value("${sql.script.delete.math.grade}")
+    private String sqlDeleteMathGrade;
+
+    @Value("${sql.script.delete.science.grade}")
+    private String sqlDeleteScienceGrade;
+
+    @Value("${sql.script.delete.history.grade}")
+    private String sqlDeleteHistoryGrade;
+
     @BeforeAll
     public static void setup(){
         request = new MockHttpServletRequest();
@@ -59,7 +84,10 @@ public class GradebookControllerTest {
 
     @BeforeEach
     public void beforeEach() {
-        jdbc.execute("insert into student(id, firstname, lastname, email_address) "+"values (1, 'Nafise', 'Namo', 'nafise@namo@gmail.com')");
+        jdbc.execute(sqlAddStudent);
+        jdbc.execute(sqlAddMathGrade);
+        jdbc.execute(sqlAddScienceGrade);
+        jdbc.execute(sqlAddHistoryGrade);
     }
 
     @Test
@@ -110,7 +138,10 @@ public class GradebookControllerTest {
 
     @AfterEach
     public void setupAfterTransaction() {
-        jdbc.execute("DELETE FROM student");
+        jdbc.execute(sqlDeleteStudent);
+        jdbc.execute(sqlDeleteMathGrade);
+        jdbc.execute(sqlDeleteScienceGrade);
+        jdbc.execute(sqlDeleteHistoryGrade);
     }
 
 }
--- 7-Spring MVC Web Apps-MVC Controller Testing/3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 7-Spring MVC Web Apps-MVC Controller Testing/3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
@@ -44,13 +45,36 @@ public class StudentAndGradeServiceTest {
     @Autowired
     private HistoryGradesDao historyGradeDao;
 
+    @Value("${sql.script.create.student}")
+    private String sqlAddStudent;
+
+    @Value("${sql.script.create.math.grade}")
+    private String sqlAddMathGrade;
+
+    @Value("${sql.script.create.science.grade}")
+    private String sqlAddScienceGrade;
+
+    @Value("${sql.script.create.history.grade}")
+    private String sqlAddHistoryGrade;
+
+    @Value("${sql.script.delete.student}")
+    private String sqlDeleteStudent;
+
+    @Value("${sql.script.delete.math.grade}")
+    private String sqlDeleteMathGrade;
+
+    @Value("${sql.script.delete.science.grade}")
+    private String sqlDeleteScienceGrade;
+
+    @Value("${sql.script.delete.history.grade}")
+    private String sqlDeleteHistoryGrade;
+
     @BeforeEach
     public void setupDatabse() {
-        jdbc.execute("insert into student(id, firstname, lastname, email_address) "+"values (1, 'Nafise', 'Namo', 'nafise@namo@gmail.com')");
-
-        jdbc.execute("insert into math_grade(student_id, grade) values (1,100.00)");
-        jdbc.execute("insert into science_grade(student_id, grade) values (1,100.00)");
-        jdbc.execute("insert into history_grade(student_id, grade) values (1,100.00)");
+        jdbc.execute(sqlAddStudent);
+        jdbc.execute(sqlAddMathGrade);
+        jdbc.execute(sqlAddScienceGrade);
+        jdbc.execute(sqlAddHistoryGrade);
     }
 
     @Test
@@ -163,14 +187,9 @@ public class StudentAndGradeServiceTest {
 
     @AfterEach
     public void setupAfterTransaction() {
-        jdbc.execute("DELETE FROM student");
-        jdbc.execute("DELETE FROM math_grade");
-        jdbc.execute("DELETE FROM science_grade");
-        jdbc.execute("DELETE FROM history_grade");
-
-        jdbc.execute("ALTER TABLE student ALTER COLUMN ID RESTART WITH 1");
-        jdbc.execute("ALTER TABLE math_grade ALTER COLUMN ID RESTART WITH 1");
-        jdbc.execute("ALTER TABLE science_grade ALTER COLUMN ID RESTART WITH 1");
-        jdbc.execute("ALTER TABLE history_grade ALTER COLUMN ID RESTART WITH 1");
+       jdbc.execute(sqlDeleteStudent);
+       jdbc.execute(sqlDeleteMathGrade);
+       jdbc.execute(sqlDeleteScienceGrade);
+       jdbc.execute(sqlDeleteHistoryGrade);
     }
 }
